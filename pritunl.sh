9584704443bbc33887dd20a36d35faaac3530387
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-05-25 15:10:34 +0300

    Update pritunl.sh

--- pritunl.sh
+++ pritunl.sh
@@ -66,3 +66,20 @@ echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgIai+2wmew3nLm4JKHnNqH7FE5p/OdPifLLE
 chmod 700 /home/admin/.ssh
 chmod 600 /home/admin/.ssh/authorized_keys
 chown -R admin:admin /home/admin/.ssh
+
+# --- Clean up ---
+cloud-init clean --logs --machine-id --seed --configs all
+rm -rf /run/cloud-init/*
+rm -rf /var/lib/cloud/*
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y autopurge
+apt-get -y clean
+rm -rf /var/lib/apt/lists/*
+journalctl --flush
+journalctl --rotate --vacuum-time=0
+find /var/log -type f -exec truncate --size 0 {} \; # truncate system logs
+find /var/log -type f -name '*.[1-9]' -delete # remove archived logs
+find /var/log -type f -name '*.gz' -delete # remove compressed archived logs
+rm -f /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*_key.pub
+fstrim --all || true
+sync
