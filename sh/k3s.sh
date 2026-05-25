9540ae2e84b190ffa0617a595e49ee17e5a80dd7
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-05-26 02:54:31 +0300

    Update k3s.sh

--- sh/k3s.sh
+++ sh/k3s.sh
@@ -9,3 +9,7 @@ sudo chmod +x /usr/local/bin/k3s
 
 curl -Lo /opt/install.sh https://get.k3s.io
 chmod +x /opt/install.sh
+
+curl -L -o k3s-airgap-images-amd64.tar.zst "https://github.com/k3s-io/k3s/releases/download/v1.35.5%2Bk3s1/k3s-airgap-images-amd64.tar.zst"
+sudo mkdir -p /var/lib/rancher/k3s/agent/images/
+sudo cp k3s-airgap-images-amd64.tar.zst /var/lib/rancher/k3s/agent/images/k3s-airgap-images-amd64.tar.zst
