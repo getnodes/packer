45fff4fa78172896cf4141b8c4c6c6effdb6f50f
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-05-28 17:15:06 +0300

    Update pritunl.sh

--- sh/pritunl.sh
+++ sh/pritunl.sh
@@ -33,4 +33,4 @@ sudo sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf'
 
-sudo systemctl enable --now pritunl mongod
+sudo systemctl enable pritunl mongod
