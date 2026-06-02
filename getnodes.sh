53951fd9c2ff6d416f8f2ec868d215306d98185c
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-06-02 14:19:15 +0300

    Update getnodes.sh

--- getnodes.sh
+++ getnodes.sh
@@ -4,6 +4,8 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
+apt-get update
+
 # --- SSH hardening ---
 sudo tee /etc/ssh/sshd_config.d/ssh-hardening.conf << EOF
 PermitRootLogin no
